24348fa250f2a6f0841d23da2cf26fc2ea2a2df0
382868331 <382868331@users.noreply.github.com>
2026-08-24 15:47:22 +0800

    challenge(012): 预发布标识原子校验

---
+++ task_semver_012_test.go
@@ -0,0 +1,22 @@
+package semver
+
+import (
+	"encoding/json"
+	"errors"
+	"sort"
+	"strings"
+	"testing"
+)
+
+var _ = json.Marshal
+var _ = errors.Is
+var _ sort.Interface
+var _ = strings.Repeat
+
+func TestTaskSemver012(t *testing.T) {
+	v := MustParse("1.2.3")
+	got, err := v.SetPrerelease("rc..1")
+	if err == nil {
+		t.Fatalf("got=%s", got.String())
+	}
+}
