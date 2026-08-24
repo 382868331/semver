47dabfba1acb5e93e034e37b03a6fbcb985ee22c
382868331 <382868331@users.noreply.github.com>
2026-08-24 15:49:55 +0800

    challenge(018): 约束OR分组解析

---
+++ task_semver_018_test.go
@@ -0,0 +1,21 @@
+package semver
+
+import (
+	"encoding/json"
+	"errors"
+	"sort"
+	"strings"
+	"testing"
+)
+
+var _ = json.Marshal
+var _ = errors.Is
+var _ sort.Interface
+var _ = strings.Repeat
+
+func TestTaskSemver018(t *testing.T) {
+	c, err := NewConstraint("<1.0.0 || >=2.0.0")
+	if err != nil || !c.Check(MustParse("2.1.0")) {
+		t.Fatalf("err=%v", err)
+	}
+}
